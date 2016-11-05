effeaed6728258ca87ed438117d6a3fe33e36b35
Vesa Laakso <laakso.vesa@gmail.com>
2016-11-05 20:19:42 +0200

    Use relative path for translationLoader

--- test/testPlugin.js
+++ test/testPlugin.js
@@ -1,11 +1,9 @@
-const { join } = require('path');
 const { transform } = require('babel-core');
 
 module.exports = function testPlugin(code) {
-  const loaderPath = join(__dirname, 'exampleTranslationLoader.js');
   const result = transform(code, {
     plugins: [['./index.js', {
-      translationLoader: loaderPath,
+      translationLoader: './test/exampleTranslationLoader',
     }]],
   });
 
