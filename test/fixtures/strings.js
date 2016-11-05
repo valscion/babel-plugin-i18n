7d1c4f37ad3e474e28ac0a1ac7f7fedc4f3bdf1f
Vesa Laakso <laakso.vesa@gmail.com>
2016-11-05 19:36:56 +0200

    Update strings test to output saner code

--- test/fixtures/strings.js
+++ test/fixtures/strings.js
@@ -1,6 +1,6 @@
-__('simple_string');
-__('two_levels.deep');
-__('nested.object.going.very.deep');
+const greetings = __('simple_string');
+const twoLevelsDeep = __('two_levels.deep');
+const veryNested = __('nested.object.going.very.deep');
 
 const numberOne = __('works_with.number');
 const booleanTrue = __('works_with.boolean');
