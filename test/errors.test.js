a4527227abe262c114a59499725534388899473e
Vesa Laakso <laakso.vesa@gmail.com>
2016-11-06 21:17:18 +0200

    chore(tests): Add tests for __arr() errors

--- test/errors.test.js
+++ test/errors.test.js
@@ -61,3 +61,34 @@ describe('__obj() errors', () => {
     });
   });
 });
+
+
+describe('__arr() errors', () => {
+  test('throwing error for missing value', () => {
+    const code = '__arr("missing")';
+    const translations = {};
+    expect(() => testPlugin(code, translations)).toThrowError(/missing/);
+  });
+
+  test('tells what part is missing', () => {
+    const code = '__arr("exists.missing.more")';
+    const translations = { exists: { foo: 'bar' } };
+    expect(() => testPlugin(code, translations)).toThrowError(/'missing'/);
+  });
+
+  [
+    { type: 'string', value: 'incorrectly string' },
+    { type: 'number', value: 123 },
+    { type: 'boolean', value: false },
+    { type: 'null', value: null },
+    { type: 'object', value: { foo: [1, 2, 3] } },
+    { type: 'deep array', value: [[1, 2], [3, 4]] },
+    { type: 'deep function', value: [() => 'is not accepted'] },
+  ].map(({ type, value }) => {
+    test(`does not allow ${type} to come through`, () => {
+      const code = '__arr("key")';
+      const translations = { key: value };
+      expect(() => testPlugin(code, translations)).toThrowError(/'key' was of incorrect type/);
+    });
+  });
+});
