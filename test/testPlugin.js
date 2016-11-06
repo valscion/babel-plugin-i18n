89791ab2641967a44e881a2ef40ed28cc1424925
Vesa Laakso <laakso.vesa@gmail.com>
2016-11-06 13:49:32 +0200

    chore(tests): Transpile tests with babel-jest

--- test/testPlugin.js
+++ test/testPlugin.js
@@ -1,10 +1,11 @@
-const { transform } = require('babel-core');
+const transform = require('babel-core').transform;
 
 module.exports = function testPlugin(code) {
   const result = transform(code, {
     plugins: [['./index.js', {
       translationLoader: './test/exampleTranslationLoader',
     }]],
+    babelrc: false,
   });
 
   return result.code;
